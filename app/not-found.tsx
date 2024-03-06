52ee4418332450b60d1ac882a3499bf082f70d1e
Erik Timmers <erik.timmers@kega.nl>
2024-03-06 15:54:47 +0100

    [MAIN] Page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            The requested page doesn't exist.
+        </div>
+    );
+};
+
+export default NotFound;
